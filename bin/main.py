b626c79320ff570cdc26be2b5181106007eabf8a
Oskar Petersen <oskar@Oskars-MacBook-Air.local>
2021-05-16 17:31:40 +0200

    improved interface for main()

--- bin/main.py
+++ bin/main.py
@@ -5,15 +5,16 @@ import time
 
 from jsonbuilder import jsonbuilder
 
-parser = argparse.ArgumentParser()
-parser.add_argument("-f", "--format")
-parser.add_argument("-t", "--table")
-parser.add_argument("-o", "--output")
-parser.add_argument("-d", "--date")
-parser.add_argument("-i", "--inspect_row", type=int)
-parser.add_argument("-p", "--profiler", action="store_true")
-parser.add_argument("-v", "--verbose", action="store_true")
-args = parser.parse_args()
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-f", "--format")
+    parser.add_argument("-t", "--table")
+    parser.add_argument("-o", "--output")
+    parser.add_argument("-d", "--date")
+    parser.add_argument("-i", "--inspect_row", type=int)
+    parser.add_argument("-p", "--profiler", action="store_true")
+    parser.add_argument("-v", "--verbose", action="store_true")
+    return parser.parse_args()
 
 """
 To run this script you must first install the project. 
@@ -25,7 +26,14 @@ python3 main.py -t sample_table.csv -f sample_format.json -v
 """
 
 
-def main():
+def main(
+    format,
+    table,
+    output=None,
+    date=None,
+    inspect_row=None,
+    verbose=False,
+):
     logging.basicConfig(
         level=logging.INFO,
         format="%(asctime)s [%(levelname)s] %(message)s",
@@ -37,37 +45,38 @@ def main():
     )
     logging.info("Process started")
 
-    with open(args.format) as f:
+    with open(format) as f:
         fmt = json.load(f)
 
     jbTree = jsonbuilder.Tree(
         fmt,
-        args.table,
-        date=args.date,
-        inspect_row=args.inspect_row,
+        table,
+        date=date,
+        inspect_row=inspect_row,
     )
-
-    if args.verbose:
-        for df in jbTree.intermediate_dfs:
-            print("\n\n", df)
 
     output_json = jbTree.build().toJson(indent=2)
 
-    if args.output:
-        with open(args.output, "w") as f:
+    if output:
+        with open(output, "w") as f:
             f.write(output_json)
 
     logging.info("Process completed")
     logging.info("Elapsed time: " + str(time.process_time()) + " seconds")
 
-    if args.verbose:
+    if verbose:
+        for df in jbTree.intermediate_dfs:
+            print("\n", df, "\n")
         if len(output_json) > 100000:
             print("Output JSON is too large to print...")
         else:
             print(output_json)
+    
+    return output_json
 
 
 if __name__ == "__main__":
+    args = parse_args()
     if args.profiler:
         import io
         import pstats
@@ -75,11 +84,18 @@ if __name__ == "__main__":
         import tabulate
 
         pr = cProfile.Profile()
-        pr.run("main()")
+        pr.run("main(args.format,args.table,date=args.date)")
         s = io.StringIO()
         ps = pstats.Stats(pr, stream=s).sort_stats(1).print_stats(30)
         rows = [x.split(maxsplit=5) for x in s.getvalue().split("\n")]
         print("\nProfiler Results:\n")
         print(tabulate.tabulate(rows[5:-3], headers="firstrow"))
     else:
-        main()
+        main(
+            args.format,
+            args.table,
+            output=args.output,
+            date=args.date,
+            inspect_row=args.inspect_row,
+            verbose=args.verbose,
+        )
